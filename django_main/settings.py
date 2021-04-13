f21dcd8b24bebccf7c1b7e31ff612dd77e24b619
raman-dev <raman@LAPTOP-T11J97H9.localdomain>
2021-04-13 18:01:39 -0400

    added secret key read from config file

--- django_main/settings.py
+++ django_main/settings.py
@@ -11,16 +11,17 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os 
+import json
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
+with open("/etc/config/django_main/config.json") as config_file:
+    config = json.load(config_file)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^94*5)38$si$6()v4ow38*u43rr9u3ul-um)r)9e0-tmm=otb8'
+SECRET_KEY = config['SECRET_KEY']#'django-insecure-^94*5)38$si$6()v4ow38*u43rr9u3ul-um)r)9e0-tmm=otb8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
