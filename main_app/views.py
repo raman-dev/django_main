f624a6a8e0acb45d328838c6baf28f625831be2d
raman-dev <raman@LAPTOP-T11J97H9.localdomain>
2021-05-06 21:53:19 -0400

    simple design to website no overcomplicated stuff

--- main_app/views.py
+++ main_app/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'main_app/index.html')#context)
+
+
+def index_alt(request):
+    return render(request,'main_app/index_alt.html')
